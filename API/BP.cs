be885f730904785bcf934613d8f8de5f66587ecd
KyeFNLeaks <kye021@hotmail.co.uk>
2020-01-01 16:21:36 +0000

    Cleaning up a bit

--- API/BP.cs
+++ API/BP.cs
@@ -1,14 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using KyeBase.DiscordRPC;
 
